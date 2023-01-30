b6a9976b1bb6298c8ae53cbf7e175eb2597830ab
gsmoff <maslovsg@ukr.net>
2023-01-30 23:53:19 +0200

    add json

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm.jsx';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', phone: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', phone: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', phone: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleAddContact = newContact =>
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+    }));
+
+  handleCheckUniqueContact = name => {
+    const { contacts } = this.state;
+
+    const isExistContact = !!contacts.find(contact => contact.name === name);
+    isExistContact && alert('Contact is already exist');
+    return !isExistContact;
+  };
+
+  handleRemoveContact = id =>
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter((contact) =>
+        contact.id !== id),
+    }));
+
+  handleFilterChange = (filter) => this.setState({ filter });
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  render() {
+    const { contacts, filter } = this.state
+    const visibleContacts = this.getVisibleContacts()
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <ContactForm
+          onAdd={this.handleAddContact}
+          onCheckUnique={this.handleCheckUniqueContact}
+        />
+
+        {contacts.length > 0 ? (
+          <ContactList
+            filter={filter}
+            contacts={visibleContacts}
+            onRemove={this.handleRemoveContact}
+            onChange={this.handleFilterChange}
+          />) : (<h3>No contacts</h3>)}
+      </>
+    );
+  }
+}
